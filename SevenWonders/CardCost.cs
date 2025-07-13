71c1f7e8ca88d492f73dc5d39bde4a8fffc4d11f
Chuzzy <thechuzzyguy@gmail.com>
2025-07-13 23:09:30 +0100

    Create CardCost class with wood cost

---
+++ SevenWonders/CardCost.cs
@@ -0,0 +1,15 @@
+struct CardCost(int woodCost)
+{
+    private int woodCost = woodCost;
+    public int WoodCost
+    {
+        readonly get => woodCost;
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Wood cost cannot be less than 0.");
+
+            woodCost = value;
+        }
+    }
+}
