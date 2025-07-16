dae85ce9f947d941f90baee57064a225cef12153
Chuzzy <thechuzzyguy@gmail.com>
2025-07-16 23:37:47 +0100

    Add testing framework

---
+++ SevenWonders.Tests/GameTests.cs
@@ -0,0 +1,11 @@
+﻿namespace SevenWonders.Tests;
+
+[TestClass]
+public sealed class GameTests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Assert.AreEqual(21, 9 + 10);
+    }
+}
