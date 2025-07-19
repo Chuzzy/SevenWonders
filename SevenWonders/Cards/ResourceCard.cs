0da5af1a9de0fdbe4f8c5b768358ba6dc17a5f35
Chuzzy <thechuzzyguy@gmail.com>
2025-07-19 03:52:02 +0100

    Create ResourceCard class

---
+++ SevenWonders/Cards/ResourceCard.cs
@@ -0,0 +1,11 @@
+namespace SevenWonders.Cards;
+
+public class ResourceCard(string name, int playerCount) : Card(name, playerCount)
+{
+    /// <summary>
+    /// An array representing the resources that this card can provide.
+    /// The indices correspond to the Resource enum values.
+    /// </summary>
+    /// <value>Mutually exclusive list of resources that this card can provide.</value>
+    public int[] ProvidedResouces { get; private set; } = new int[7];
+}
