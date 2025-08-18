c571ad6ab9c08b083734b08ee812fac05b0945d8
Chuzzy <thechuzzyguy@gmail.com>
2025-08-18 03:26:14 +0100

    Change type of CardCost.Costs to ResourceList

--- SevenWonders/CardCost.cs
+++ SevenWonders/CardCost.cs
@@ -1,7 +1,7 @@
 namespace SevenWonders;
 
-public readonly struct CardCost(int[] costs, ChainingSymbol chainingSymbol = ChainingSymbol.None)
+public readonly struct CardCost(ResourceList costs, ChainingSymbol chainingSymbol = ChainingSymbol.None)
 {
-    public int[] Costs { get; } = costs;
+    public ResourceList Costs { get; } = costs;
     public ChainingSymbol ChainingSymbol { get; } = chainingSymbol;
 }
