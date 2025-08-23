5a08718789c367978334735b75de930b3adf6b02
Chuzzy <thechuzzyguy@gmail.com>
2025-08-23 16:00:11 +0100

    Add CardCost.Coins

--- SevenWonders/CardCost.cs
+++ SevenWonders/CardCost.cs
@@ -1,8 +1,9 @@
 namespace SevenWonders;
 
-public readonly struct CardCost(ResourceList costs, ChainingSymbol chainingSymbol = ChainingSymbol.None)
+public readonly struct CardCost(ResourceList costs, int coins = 0, ChainingSymbol chainingSymbol = ChainingSymbol.None)
 {
     public ResourceList Costs { get; } = costs;
+    public int Coins { get; } = coins;
     public ChainingSymbol ChainingSymbol { get; } = chainingSymbol;
 
     public static CardCost None => new CardCost();
