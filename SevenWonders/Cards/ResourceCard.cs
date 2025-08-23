5a2b885f57a5e39922b1854b303f0bbe441fa7c1
Chuzzy <thechuzzyguy@gmail.com>
2025-08-23 13:51:53 +0100

    Add providedResources parameter to ResourceCard

--- SevenWonders/Cards/ResourceCard.cs
+++ SevenWonders/Cards/ResourceCard.cs
@@ -1,27 +1,27 @@
 namespace SevenWonders.Cards;
 
-public class ResourceCard(string name, int playerCount, int age, CardCost cardCost) : Card(name, playerCount, age, cardCost)
+public class ResourceCard(string name, int playerCount, int age, CardCost cardCost, ResourceList providedResources) : Card(name, playerCount, age, cardCost)
 {
     /// <summary>
     /// An array representing the resources that this card can provide.
     /// The indices correspond to the Resource enum values.
     /// </summary>
     /// <value>Mutually exclusive list of resources that this card can provide.</value>
-    public int[] ProvidedResouces { get; private set; } = new int[7];
+    public ResourceList ProvidedResouces { get; private set; } = providedResources;
 
     public override CardColor Color
     {
         get
         {
             // Must be a yellow card if it provides at least three resources.
-            if (ProvidedResouces.Count(r => r > 0) >= 3)
+            if (ProvidedResouces.NumberOfDifferentResources >= 3)
             {
                 return CardColor.Yellow;
             }
             // Otherwise, if it provides any of either glass, paper or cloth, it is a gray card.
-            else if (ProvidedResouces[(int)Resource.Glass] > 0 ||
-                     ProvidedResouces[(int)Resource.Paper] > 0 ||
-                     ProvidedResouces[(int)Resource.Cloth] > 0)
+            else if (ProvidedResouces.Glass > 0 ||
+                     ProvidedResouces.Paper > 0 ||
+                     ProvidedResouces.Cloth > 0)
             {
                 return CardColor.Gray;
             }
