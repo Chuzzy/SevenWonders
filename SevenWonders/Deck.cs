802c8221d8ca73fe647f5944b41c17a82c180f21
Chuzzy <thechuzzyguy@gmail.com>
2025-08-30 23:07:15 +0100

    Add blue cards to Deck

--- SevenWonders/Deck.cs
+++ SevenWonders/Deck.cs
@@ -37,6 +37,28 @@ static class Deck
         { new ResourceCard("Press", 2, CardCost.None, new ResourceList { Paper = 1 }), PlayerCount.Three | PlayerCount.Five },
         { new ResourceCard("Loom", 2, CardCost.None, new ResourceList { Cloth = 1 }), PlayerCount.Three | PlayerCount.Five },
         #endregion
+
+        #region Blue Cards - Age I
+        { new CivilianCard("Altar", 1, CardCost.None, 3), PlayerCount.Three | PlayerCount.Five },
+        { new CivilianCard("Theater", 1, CardCost.None, 3), PlayerCount.Three | PlayerCount.Six },
+        { new CivilianCard("Well", 1, CardCost.None, 3), PlayerCount.Four | PlayerCount.Seven },
+        { new CivilianCard("Baths", 1, new CardCost(new ResourceList { Stone = 1 }), 3), PlayerCount.Four | PlayerCount.Seven },
+        #endregion
+
+        #region Blue Cards - Age II
+        { new CivilianCard("Courthouse", 2, new CardCost(new ResourceList { Clay = 2, Cloth = 1 }), 4), PlayerCount.Three | PlayerCount.Five },
+        { new CivilianCard("Temple", 2, new CardCost(new ResourceList { Wood = 1, Clay = 1, Glass = 1 }), 4), PlayerCount.Three | PlayerCount.Six },
+        { new CivilianCard("Statue", 2, new CardCost(new ResourceList { Ore = 2, Wood = 1 }), 4), PlayerCount.Three | PlayerCount.Seven },
+        { new CivilianCard("Aqueduct", 2, new CardCost(new ResourceList { Stone = 3 }), 5), PlayerCount.Three | PlayerCount.Seven },
+        #endregion
+        
+        #region Blue Cards - Age III
+        { new CivilianCard("Gardens", 3, new CardCost(new ResourceList { Clay = 2, Wood = 1 }), 5), PlayerCount.Three | PlayerCount.Four },
+        { new CivilianCard("Senate", 3, new CardCost(new ResourceList { Wood = 2, Stone = 1, Clay = 1 }), 6), PlayerCount.Three | PlayerCount.Five },
+        { new CivilianCard("Town Hall", 3, new CardCost(new ResourceList { Stone = 3, Glass = 1 }), 6), PlayerCount.Three | PlayerCount.Six },
+        { new CivilianCard("Pantheon", 3, new CardCost(new ResourceList { Clay = 2, Ore = 1, Glass = 1, Paper = 1, Cloth = 1 }), 7), PlayerCount.Three | PlayerCount.Six },
+        { new CivilianCard("Palace", 3, new CardCost(new ResourceList { Wood = 1, Stone = 1, Ore = 1, Clay = 1, Glass = 1, Paper = 1, Cloth = 1 }), 8), PlayerCount.Three | PlayerCount.Seven }
+        #endregion
     };
 
 
