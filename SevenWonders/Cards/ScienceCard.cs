f3d4ebfd42671ad43bf0517857c6ab42356d5532
Chuzzy <thechuzzyguy@gmail.com>
2025-08-31 03:54:28 +0100

    Create ScienceCard class

---
+++ SevenWonders/Cards/ScienceCard.cs
@@ -0,0 +1,8 @@
+namespace SevenWonders.Cards;
+
+public class ScienceCard(string name, int age, CardCost cardCost, ScienceSymbol symbol) : Card(name, age, cardCost)
+{
+    public ScienceSymbol Symbol { get; private set; } = symbol;
+
+    public override CardColor Color => CardColor.Green;
+}
