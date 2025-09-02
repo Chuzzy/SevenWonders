a430c6c02bfa3fd205fd09b8760af6bdaa190a24
Chuzzy <thechuzzyguy@gmail.com>
2025-09-02 23:25:39 +0100

    Add Flags attribute to CardColor enum

--- SevenWonders/CardColor.cs
+++ SevenWonders/CardColor.cs
@@ -1,12 +1,13 @@
 namespace SevenWonders;
 
+[Flags]
 public enum CardColor
 {
-    Brown,
-    Gray,
-    Yellow,
-    Green,
-    Blue,
-    Red,
-    Purple,
+    Brown = 1,
+    Gray = 2,
+    Yellow = 4,
+    Green = 8,
+    Blue = 16,
+    Red = 32,
+    Purple = 64,
 }
