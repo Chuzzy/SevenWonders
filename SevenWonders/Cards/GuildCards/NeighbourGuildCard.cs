05f474b439bcfc1697b087d724016dacfbaf85f9
Chuzzy <thechuzzyguy@gmail.com>
2025-09-02 01:14:28 +0100

    Add NeighbourGuildCard.PointsPerCard

--- SevenWonders/Cards/GuildCards/NeighbourGuildCard.cs
+++ SevenWonders/Cards/GuildCards/NeighbourGuildCard.cs
@@ -1,6 +1,8 @@
 namespace SevenWonders.Cards.GuildCards;
 
-public class NeighbourGuildCard(string name, CardCost cost, CardColor targetColor) : GuildCard(name, cost)
+public class NeighbourGuildCard(string name, CardCost cost, CardColor targetColor, int pointsPerCard = 1) : GuildCard(name, cost)
 {
     public CardColor TargetColor { get; private set; } = targetColor;
+
+    public int PointsPerCard { get; private set; } = pointsPerCard;
 }
