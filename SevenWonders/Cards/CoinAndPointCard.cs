50a5c2dfa177b407356e597a216701d769747a3e
Chuzzy <thechuzzyguy@gmail.com>
2025-09-03 00:00:34 +0100

    Add Shipowners Guild card

--- SevenWonders/Cards/CoinAndPointCard.cs
+++ SevenWonders/Cards/CoinAndPointCard.cs
@@ -12,10 +12,12 @@ public class CoinAndPointCard(string name, int age, CardCost cardCost, CardColor
 
     public int ProvidedPoints => targetColor switch
     {
+        CardColor.Brown | CardColor.Gray | CardColor.Purple or
         CardColor.Yellow or CardColor.Brown or CardColor.Red => 1,
         CardColor.Gray => 2,
         _ => 0,
     };
 
-    public override CardColor Color => CardColor.Yellow;
+    public override CardColor Color =>
+        targetColor == (CardColor.Brown | CardColor.Gray | CardColor.Purple) ? CardColor.Purple : CardColor.Yellow;
 }
