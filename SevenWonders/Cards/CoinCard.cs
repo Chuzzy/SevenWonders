0390fac62081be93613d3090a78ea324ab5bf921
Chuzzy <thechuzzyguy@gmail.com>
2025-09-02 18:47:47 +0100

    Add provided coins field to CoinCard

--- SevenWonders/Cards/CoinCard.cs
+++ SevenWonders/Cards/CoinCard.cs
@@ -1,6 +1,8 @@
 namespace SevenWonders.Cards;
 
-public class CoinCard(string name, int age, CardCost cardCost, ChainingSymbol providedChainingSymbol = ChainingSymbol.None) : Card(name, age, cardCost, providedChainingSymbol)
+public class CoinCard(string name, int age, CardCost cardCost, int providedCoins, ChainingSymbol providedChainingSymbol = ChainingSymbol.None) : Card(name, age, cardCost, providedChainingSymbol)
 {
+    public int ProvidedCoins { get; private set; } = providedCoins;
+
     public override CardColor Color => CardColor.Yellow;
 }
